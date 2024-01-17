1539ac852804c52c69fb0fd2e45ef68d5086bdac
mbiopeter <mbiopeter401@gmail.com>
2024-01-17 11:23:06 +0300

    completed

--- Python_app.py
+++ Python_app.py
@@ -11,7 +11,7 @@ email_password = APP_PASSWORD
 
 email_receiver = RECEIVER_EMAIL
 
-subject = 'Pyhton Programming Language'
+subject = 'Python Programming Language'
 
 body ="""
 Python, created by Guido van Rossum, emerged in the late 1980s as a versatile and readable programming language. Guido, aiming for a language that prioritized code readability and simplicity, released Python's first version, 0.9.0, in 1991. Its name was inspired by the British comedy group Monty Python, highlighting the language's emphasis on humor and fun. Python steadily gained popularity due to its clear syntax and a robust standard library, attracting developers across various domains. The release of Python 2 in 2000 and Python 3 in 2008 marked significant milestones, with Python 3 addressing language inconsistencies. Python's versatility propelled it to become a dominant force in web development, data science, artificial intelligence, and more. Its community-driven development and commitment to simplicity have sustained Python's widespread adoption, making it a go-to language for both beginners and seasoned developers alike. 
