b4b3f731217fa392a5ce0326676d3adea7db7fae
mbiopeter <mbiopeter401@gmail.com>
2024-01-17 10:45:59 +0300

    enviroment set up

--- Python_app.py
+++ Python_app.py
@@ -1,3 +1,8 @@
 #Go over to your Gmail account and setup 2 factor authentication
 #generate app password
 #create a function to send the email
+import os
+from email.message import EmailMessage
+
+email_sender = 'mbiopeter401@gmail.com'
+email_password = os.environ.get('APP_PASSWORD')
